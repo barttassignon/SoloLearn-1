cdda9147ed753757f111914ace12f172f4f5a8c8
Bart Tassignon <bart.tassignon@student.ehb.be>
2022-06-08 20:30:57 +0200

    2

---
+++ SoloLearn 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SoloLearn_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14;
+            double radius;
+            radius = Convert.ToDouble(Console.ReadLine());
+
+
+            double output = pi * (radius * radius);
+            Console.WriteLine(output);
+        }
+
+    }
+}
